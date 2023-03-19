08c686ef64ca257542db00b1b2c427b0635d6be2
anujmani <anujmani.shrestha@gmail.com>
2023-03-19 15:43:36 +0545

    Some changes

---
+++ src/day2.java
@@ -0,0 +1,14 @@
+public class day2 {
+    public static void main(String[]args){
+        System.out.println("In order to setup the development environment of java ");
+        //Assuming you have installed Java in c:\Program Files\java\jdk directory −
+        //
+        //Right-click on 'My Computer' and select 'Properties'.
+        //
+        //Click the 'Environment variables' button under the 'Advanced' tab.
+        //
+        //Now, alter the 'Path' variable so that it also contains the path to the Java executable. Example, if the
+        // path is currently set to 'C:\WINDOWS\SYSTEM32', then change your path to read 'C:\WINDOWS\SYSTEM32;
+        // c:\Program Files\java\jdk\bin'.
+    }
+}
