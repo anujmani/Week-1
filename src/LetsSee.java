c30e601c4e6b2af771600b3a081e356f856c2431
anujmani <80209512+anujmani@users.noreply.github.com>
2023-03-20 12:18:09 +0545

    Delete LetsSee.java

--- src/LetsSee.java
+++
@@ -1,9 +0,0 @@
-public class LetsSee {
-    public static void main (String[]args){
-        System.out.println("Herumla");
-    }
-    //to create a new branch
-    //git branch name
-    //git checkout name
-    //git push origin name
-}
