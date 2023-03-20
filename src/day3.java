23ef6a152606f36f72634bfa185cc0ce6f8466a5
anujmani <anujmani.shrestha@gmail.com>
2023-03-20 11:45:32 +0545

    operators

---
+++ src/day3.java
@@ -0,0 +1,28 @@
+public class day3 {
+    public static void main(String[] args) {
+        //operators
+        int a= 4;
+        int b = 5;
+        String msg="When";
+        String check="Why";
+        if(a==b){
+            System.out.println("Equal");
+
+        } else if (a<b) {
+            System.out.println("less a");
+
+        }
+        else if(b<a){
+            System.out.println("less b");
+        }
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a/b);
+        System.out.println(a*b);
+        System.out.println(a%b);
+        System.out.println(a&b);
+        if(a==1 && b==1){
+            a++;
+         }
+    }
+}
