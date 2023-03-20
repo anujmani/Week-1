e029cf603ee3176a60298df392a355ba3f3b4916
anujmani <anujmani.shrestha@gmail.com>
2023-03-20 09:40:14 +0545

    task1

--- src/LetsSee.java
+++ src/LetsSee.java
@@ -2,4 +2,8 @@ public class LetsSee {
     public static void main (String[]args){
         System.out.println("Herumla");
     }
+    //to create a new branch
+    //git branch name
+    //git checkout name
+    //git push origin name
 }
---
+++ src/day2datatypes.java
@@ -0,0 +1,16 @@
+public class day2datatypes {
+        public static void main(String[] args) {
+            int a = 3;
+            String msg = "Hello";
+            float dec= 2.123f;
+            System.out.println(msg+","+a+"+"+dec);
+                    System.out.println("Hello world!");
+            //There are two types of data types
+            // They are : Primitive and non-primitive
+            //In primitive datatype there are boolean, char and many more
+            //In non primitive datatype there are string, arrray etc.
+
+        }
+    }
+
+}
